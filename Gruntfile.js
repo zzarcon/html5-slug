38b43ac78db61643710b55f968feaaa9df5b438f
Hector Zarco <hezarco@gmail.com>
2014-04-21 23:35:36 +0200

    project structure

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    watch: {
+      game: {
+        files: ['game/*.js'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      game: {
+        options: {
+          port: 9092,
+          base: ['game']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect:game', 'watch']);
+};
