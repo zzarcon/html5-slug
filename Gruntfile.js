d39863c3a4479395b86e061222bb0f42e60a2bf7
Hector Zarco <hezarco@gmail.com>
2014-04-22 08:43:08 +0200

    playing with phaser

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     watch: {
       game: {
-        files: ['game/*.js'],
+        files: ['game/*'],
         options: {
           livereload: true
         }
