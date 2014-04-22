1b04555f47d811b108d9cb38ebc44c96ec4eade3
Hector Zarco <hezarco@gmail.com>
2014-04-23 00:04:45 +0200

    Check collisions and animations

--- game/app.js
+++ game/app.js
@@ -1,5 +1,5 @@
-(function(){
-  var platforms, player;
+  (function(){
+  var platforms, player, cursors;
   var config = {
     width: 800,
     height: 600
@@ -47,19 +47,46 @@
     // The player and its settings
     player = game.add.sprite(32, game.world.height - 150, 'dude');
 
-    //  We need to enable physics on the player
+    //We need to enable physics on the player
     game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
+    //Player physics properties. Give the little guy a slight bounce.
     player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
+    player.body.gravity.y = 700;
     player.body.collideWorldBounds = true;
 
-    //  Our two animations, walking left and right.
+    //Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    //Cursors
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
   function update() {
+    //Collide the player and the stars with the platforms
+    game.physics.arcade.collide(player, platforms);
+
+    //Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+        //Move to the left
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    } else if (cursors.right.isDown) {
+        //Move to the right
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    } else {
+        //Stand still
+        player.animations.stop();
+        player.frame = 4;
+    }
+
+    //Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -350;
+    }
   }
 })();
