70af4378b019ee224ea6d24dfca0c5ac1ddac5f3
Hector Zarco <hezarco@gmail.com>
2014-04-26 20:49:57 +0200

    Make Marco lookingUp animation

--- game/init_game.js
+++ game/init_game.js
@@ -1,55 +1,47 @@
 (function() {
     var platforms, player, cursors, mummy, ground;
+    var mission = {
+        src: 'assets/mission-1.png',
+        width: 4088,
+        height: 336
+    };
     var config = {
         width: 800,
         height: 600
     };
-    var game = new Phaser.Game(config.width, config.height, Phaser.AUTO, '', {
+    var game = new Phaser.Game(config.width, mission.height, Phaser.AUTO, '', {
         preload: preload,
         create: create,
         update: update
     });
-
     function preload() {
-        game.load.image('mission1', 'assets/mission-1.png');
-        game.load.image('ground', 'assets/platform.png');
+        game.load.image('mission1', mission.src);
+        game.load.image('ground', 'assets/platform2.png');
         game.load.atlasXML('marco', 'assets/marco.png', 'assets/marco.xml');
         game.load.spritesheet('mummy', 'assets/mummy.png', 37, 45, 18);
     }
 
     function create() {
 
-        game.world.setBounds(0, 0, 4088, config.height);
+        game.world.setBounds(0, 0, mission.width, config.height);
         //We're going to be using physics, so enable the Arcade Physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         game.add.sprite(0, 200, 'mission1');
-        //The platforms group contains the ground and the 2 ledges we can jump on
+
         platforms = game.add.group();
-
-        //We will enable physics for any object that is created in this group
         platforms.enableBody = true;
-        // debugger
-        // Here we create the ground.
-        ground = platforms.create(0, game.world.height - 64, 'ground');
 
-        //Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground = platforms.create(0, game.world.height - 90, 'ground');
         ground.scale.setTo(4, 2);
-
-        //This stops it from falling away when you jump on it
         ground.body.immovable = true;
 
-        //Now let's create two ledges
-        // var ledge = platforms.create(400, 400, 'ground');
-
-        // ledge.body.immovable = true;
-
+        var ledge = platforms.create(0, 480, 'ground');
+        ledge.body.immovable = true;
         // ledge = platforms.create(-150, 250, 'ground');
-
         // ledge.body.immovable = true;
 
-        // The player and its settings
-        player = game.add.sprite(300, game.world.height - 150, 'marco');
+        player = game.add.sprite(750, game.world.height - 170, 'marco');
 
         game.camera.follow(player);
         //We need to enable physics on the player
@@ -61,13 +53,23 @@
         player.body.collideWorldBounds = true;
 
         //Animations
-        player.animations.add('walk', null, 10);
 
+        player.animations.add('walk', [4,5,6], 10);
+
+        player.animations.add('lookingUp', [13,12,11,10], 10, false);
         //Mummy
         // createMummy();
 
         //Cursors
-        cursors = game.input.keyboard.createCursorKeys();
+        cursors = createCursors();
+    }
+
+    function createCursors() {
+        var c = game.input.keyboard.createCursorKeys();
+        c.jump = game.input.keyboard.addKey(Phaser.Keyboard.D);
+        c.fire = game.input.keyboard.addKey(Phaser.Keyboard.A);
+
+        return c;
     }
 
     function update() {
@@ -78,34 +80,41 @@
         //Reset the players velocity (movement)
         player.body.velocity.x = 0;
 
+        if (cursors.fire.isDown) {
+            console.log('fire');
+        }
+
+        //Cursor keys
         if (cursors.left.isDown) {
             //Set left direction
             if (player.scale.x != -1) {
                 player.scale.x = -1;
             }
-            //Move to the left
             player.body.velocity.x = -150;
-            // player.scale.x *= -1
-            player.flipped = true;
             player.animations.play('walk');
         } else if (cursors.right.isDown) {
             //Set right direction
             if (player.scale.x != 1) {
                 player.scale.x = 1;
             }
-            //Move to the right
             player.body.velocity.x = 150;
-            player.flipped = false;
             player.animations.play('walk');
+        } else if (cursors.up.isDown) {
+            if (player.animations.currentFrame.index === 10) {
+                player.animations.stop('lookingUp');
+            } else {
+                player.animations.play('lookingUp');
+            }
+        } else if (cursors.down.isDown) {
+
         } else {
-            //Stand still
             player.animations.stop();
             player.frame = 7;
         }
 
         //Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && player.body.touching.down) {
-            player.body.velocity.y = -500;
+        if (cursors.jump.isDown && player.body.touching.down) {
+            player.body.velocity.y = -300;
         }
     }
 
